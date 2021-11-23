41028782243295535ce8d5467cca4fcf18e2b8cf
Eric Scaperrotta <91492178+EricScaperrotta@users.noreply.github.com>
2021-11-23 18:33:19 -0500

    Update Code-Challenge.js

--- Code-Challenge.js
+++ Code-Challenge.js
@@ -1,5 +1,10 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+  let sum =0;
+
+for(i = 0; i < array.length; i++){
+  sum += array[i]
+};
+return sum;
 };
 
 const findFrequency = function(array) {
@@ -19,5 +24,34 @@ const removeParenth = function(str) {
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+ const arrayOfLetters = str.split('');
+let sum = 0//keep track of sum
+for(i = 0; i < arrayOfLetters.length; i++){
+  switch (arrayOfLetters[i]) {
+    case 'a' || 'e' || 'i' || 'o' || 'u' || 'l' || 'n' || 'r' || 's' || 't':
+      sum += 1;
+      break;
+    case 'd' || 'g':
+      sum += 2;
+      break;
+    case 'b' || 'c' || 'm' || 'p':
+      sum += 3;
+      break;
+    case 'f' || 'h' || 'v' || 'w' || 'y':
+      sum += 4;
+      break;
+    case 'k':
+      sum += 5;
+      break;
+    case 'j' || 'x':
+      sum += 8;
+      break;
+    case 'q' || 'z':
+      sum += 10;
+      break;
+  } 
 };
+
+return sum;//return sum
+};
+
